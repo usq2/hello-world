bbd0626295a1384cc955839cc32d6fdcfbfce27a
Riyuksh <56798573+Riyuksh@users.noreply.github.com>
2019-11-09 23:25:57 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,93 +1,90 @@
+//Queues (linear)
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #define MAXSIZE 4
-//Defining a Stack
-typedef struct stack
+//Defining Queue
+typedef struct
 {
-    char item[MAXSIZE][10];
-    int top;
-}STACK;
-//Checking Whether the Stack is full
-int isfull(STACK s)
+    char items[MAXSIZE][20];
+    int f,r;
+}QUEUE;
+// Overflow Condition
+int isfull(QUEUE q)
 {
-    if(s.top==MAXSIZE-1)
+    if(q.r==MAXSIZE-1)
         return 1;
     return 0;
 }
-//Checking whether the Stack is empty
-int isempty(STACK s)
+int isempty(QUEUE q)
 {
-    if(s.top==-1)
+    if(q.f==-1)
         return 1;
     return 0;
 }
-//Functions
-//Inserting an item into the stack
-void push(STACK *s,char data[])
+//Insertion
+void push(QUEUE *q,char data[])
 {
-  strcpy(s->item[++s->top],data);
+    strcpy(q->items[++q->r],data);
+    printf("\nThe Character inserted is %s",data);
+    if(q->f==-1)
+        q->f=0;
 }
-//Deleting an item from the stack
-char * pop(STACK *s)
+//Delete
+char *qdelete(QUEUE *q)
 {
-    return(s->item[s->top--]);
+    char data[20];
+    strcpy(data,q->items[q->f]);
+    if(q->f==q->r)
+        q->f=q->r=-1;
+    else
+        q->f++;
+    return data;
 }
 //Display
-void display(STACK s)
+void display(QUEUE q)
 {
     int i;
-    printf("\n The Stack Contents :\n BOS:-->");
-    for(i=0;i<=s.top;i++)
-        printf("%s-->",s.item[i]);
-    printf("TOS\n");
+    printf("\n Queue Contents : \n F-->");
+    for(i=q.f;i<=q.r;i++)
+        printf("%s-->",q.items[i]);
+    printf("R");
 }
-//Main Function
 int main()
 {
+    QUEUE q;
     int choice;
-    char data[20];
-    STACK s;
-    s.top=-1;
-    printf("\n 1:PUSH\t 2:POP\t 3:DISPLAY\t4:EXIT\n");
+    char ch[20];
+    q.r=q.f=-1;
     while(1)
     {
-        printf("\n Enter the choice : \t");
+        printf("\n 1:Insert\t 2:Delete\t 3:Display\t 4:Exit\n");
+        printf("Enter the choice :\t");
         scanf("%d",&choice);
-         switch(choice)
-         {
-             case 1 : if(isfull(s))
-                        {
-                            printf("\n Full Stack \n");
-                           break;
-                        }
-                      else
-                      {
-                        printf("\nEnter the data to be inserted : \t");
-                        scanf("%s",data);
-                        push(&s,data);
-                      }
-                  break;
-             case 2 : if(isempty(s))
-                        {
-                            printf("\n Empty Stack \n");
-                            break;
-                        }
-                      else
-                      {
-                          printf("\n The data to be popped out is %s",pop(&s));
-                      }
-                  break;
-              case 3 : if(isempty(s))
-                        printf("\n Empty Stack \n");
-                      else
-                      {
-                          display(s);
-                      }
-                  break;
-              case 4 : exit(0);
-              default : printf("\n Invalid Input \n");
-         }
+            switch(choice)
+            {
+                case 1 : if(isfull(q))
+                            printf("\n OverFlow");
+                         else
+                            {
+                            printf("Enter the character :");
+                            scanf("%s",ch);
+                            push(&q,ch);
+                            }
+                        break;
+                case 2 : if(isempty(q))
+                            printf("\n QUEUE UNDERFLOW\n");
+                         else
+                            printf("\n %s is delete off from the queue",qdelete(&q));
+                        break;
+                case 3 : if(isempty(q))
+                             printf("\n QUEUE UNDERFLOW\n");
+                         else
+                             display(q);
+                         break;
+                case 4 : exit(0);
+                default : printf("INVALID INPUT\n");
+                    }
     }
-    return 0;
+    return(0);
 }
